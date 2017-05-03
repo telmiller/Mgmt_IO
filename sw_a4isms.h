ab02bc29f605a7ce93740b2a7ca1b4d9a080c2a9
Tom Miller <tmillr@outlook.com>
2017-05-02 21:26:44 -0700

    Initial checkin

---
+++ sw_a4isms.h
@@ -0,0 +1,74 @@
+/* Contribution to SW Dev Mgmt from the Commercial Side */
+/* For docs and explanation see: http://millr.us/&p=742 */
+
+/*
+* Any great piece of software has been re-written
+* at least three times
+*/
+
+/*
+* When debugging, try to think of the
+* application organically, a complex
+* but soulless creature
+*/
+
+/*
+* QA is the buffer at the end of the development
+* schedule for when development slip
+*/
+
+/*
+* Being too DRY will leave you in the DeSERT.
+*/
+
+/*
+* Third Law of Open Source Software Development:
+* For every open source library, there is an
+* equal and opposite implementation.
+*/
+
+#include <sales_io.h>
+/*
+* returns data on customer requirements,
+* priorities, and value creating features.
+* Returned objects should be rejected
+* if no priority or value information is
+* included, however because customers don't
+* tell you everything in the first meetings,
+* objects may otherwise be incomplete.
+* Objects may contain surprising data
+* about customer context so your parser
+* should be open and flexible. Function
+* calls to this module should not contain
+* an argument.  But, if you're not satisfied,
+* it's appropriate to send multiple
+* retries until sufficient data exists
+* to complete the operation.
+*/
+
+#include <marketing_io.h>
+/*
+* Data you send to this function will be formatted
+* to fit inside a story telling framework.
+* Standalone data that doesn't fit into
+* this framework will be saved to /dev/null.
+* However, the beauty of most marketing frameworks
+* is that they are easily extensible.
+* If you find important data is getting dropped,
+* you can contribute a model-view-controller
+* to enable the marketing framework to extend
+* their UI.
+*/
+
+#include <top_customers_io.h>
+/*
+* Sessions should be initiated through the
+* Account Executive proxy, which establishes
+* the connection but from then on, that session
+* should be point-to-point. The protocol handler
+* should accept all kinds of data, but pay
+* attention to inbound vs outbound bit rates.
+* If the ratio of outbound vs inbound data
+* exceeds 3:4 or goes below 1:4, something
+* is going wrong
+*/
